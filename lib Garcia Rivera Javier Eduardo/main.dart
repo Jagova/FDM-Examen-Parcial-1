4d6542c7cd00632f76a2d504f2b1f454c7074d17
Javierszn <javier.gariv@hotmail.com>
2026-09-15 08:03:36 -0600

    Examen Parcial 1 y Propuesta - Garcia Rivera Javier

---
+++ lib Garcia Rivera Javier Eduardo/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mi_primera_app/features/home/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness Tracker',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
