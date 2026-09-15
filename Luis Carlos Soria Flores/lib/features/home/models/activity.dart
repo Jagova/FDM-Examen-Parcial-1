0f260a1969b4f9f95755f3ee1c530919ec6d82d4
Luis Soria <luiscarlossoriaflores@gmail.com>
2026-09-15 08:02:59 -0600

    Examen Luis Carlos Soria Flores

---
+++ Luis Carlos Soria Flores/lib/features/home/models/activity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Activity{
+  final String title;
+  final String subtitle;
+  final String trailingtext;
+  final IconData icon;
+
+  Activity({
+    required this.title,
+    required this.subtitle,
+    required this.trailingtext,
+    required this.icon,
+  });
+}
